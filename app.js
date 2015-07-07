35ad814b8ef20e0072734bd93a5103bc63235c78
stofstik <verstegen.daan@gmail.com>
2015-07-07 14:11:18 +0200

    Added comments

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ var request = require('request');
 var moment = require('moment');
 var dtd = require('degtodir');
 
-// log("started!");
-
+// function to return formatted 'now' date
 function dateFormat(date) {
     return moment(date).format("YYYY-MM-DD HH:mm:ss");
 }
 
-function log (string){
-    console.log(dateFormat(new Date) + " " +  string);
+// print log string to console
+function log(string) {
+    console.log(dateFormat(new Date) + " " + string);
 }
 
+// function which prints the given argument to a file called wind.log
 function appendLine(line) {
     var file = "wind.log";
     line += "\r\n";
@@ -28,6 +29,7 @@ function appendLine(line) {
     });
 }
 
+// make a call to the api
 function apiCall(retry) {
     // declare API stuffs
     // var apiKey = "44b456335f8c7ab81a09cc743bf5b332";
@@ -40,6 +42,7 @@ function apiCall(retry) {
         url: apiUri,
     };
 
+    // set up 'getData', this is part of 'Request'
     function getData(error, response, body) {
         if (!error && response.statusCode == 200) {
             // log(apiUri);
@@ -64,6 +67,18 @@ function apiCall(retry) {
     request(options, getData);
 }
 
+// poll the api, if we did not get wind data we put apiCall on the event queue
+function pollApi() {
+    apiCall(function () {
+        // we did not get wind direction we should retry... And wait for a while
+        setTimeout(function () {
+            log("did not get wind direction, retrying...");
+            pollApi(); // call wrapper function to place apiCall() on the event queue again
+        }, 1000 * 60); // wait for 1 minute
+    });
+}
+
+// display data on console
 function displayData(error, data) {
     if (!error) {
         log(dtd.degToDir(data.wind.deg) + " " + data.wind.speed);
@@ -72,6 +87,7 @@ function displayData(error, data) {
     }
 }
 
+// create string with data, convert wind degrees to direction and print to file
 function saveData(data) {
     var line = "";
     line += dateFormat(new Date());
@@ -83,14 +99,5 @@ function saveData(data) {
     appendLine(line);
 }
 
-function pollApi(){
-    apiCall(function () {
-        // we did not get data we should retry... With an interval
-        setTimeout(function () {
-            log("did not get data, retrying...");
-            pollApi(); // call wrapper function to place apiCall() on the event queue again
-        }, 10000);
-    });
-}
-
+// run the program
 pollApi();
